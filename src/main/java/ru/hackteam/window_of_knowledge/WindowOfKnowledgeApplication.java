9d90288b2753aeb026418ad757ff2bea614daca9
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2024-11-26 20:06:40 +0300

    init commit

---
+++ src/main/java/ru/hackteam/window_of_knowledge/WindowOfKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package ru.hackteam.window_of_knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowOfKnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowOfKnowledgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hackteam/window_of_knowledge/WindowOfKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hackteam.window_of_knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowOfKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
